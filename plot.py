946fa3eb79c22db78011aa87ce1c3b2a9bc74d74
ishimwebernard <isbernard2001@gmail.com>
2026-02-05 15:11:36 +0200

    mend

--- plot.py
+++ plot.py
@@ -1,19 +1,18 @@
-# Import libraries
-import pandas as pd ## for dataset management
-import matplotlib.pyplot as plt ## for data visualization
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-# Read data
+
 data = pd.read_csv('FungusLocations.csv').dropna()
-# Build K-means model with 3 clusters
-km = KMeans(n_clusters=3, n_init='auto')
-km.fit(data)
-# Calculate silhouette score
-silhouette_avg = silhouette_score(data, km.labels_)
-print("Silhouette Score:", round(silhouette_avg,2))
-# Visualize the result of k-Means clustering using matplotlib
-plt.scatter(data['East'], data['North'], c=km.labels_, cmap='viridis')
+
+kmeans = KMeans(n_clusters=3, n_init='auto')
+kmeans.fit(data)
+
+silhouette_avg = silhouette_score(data, kmeans.labels_)
+print('Silhouette score', round(silhouette_avg, 2))
+
+plt.scatter(data['East'], data['North'], c=kmeans.labels_, cmap='viridis')
 plt.xlabel('East')
 plt.ylabel('North')
-plt.title('k-Means Clustering Result')
+plt.title('KMeans Clustering Results')
 plt.show()
