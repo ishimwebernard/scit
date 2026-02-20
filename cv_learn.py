a9af9e6901803d2b2d4c47c255dd886f9f320431
ishimwebernard <isbernard2001@gmail.com>
2026-02-20 12:02:33 +0200

    mend

--- cv_learn.py
+++ cv_learn.py
@@ -1,4 +1,43 @@
+import os
 import cv2
+
+video_path = os.path.join('.', "cutout.mp4")
+video = cv2.VideoCapture(video_path)
+
+rel = True
+counter = 1
+while rel:
+    rel, frame = video.read()
+
+    if rel:
+        cv2.imwrite(os.path.join('.', "cutout" + str(counter) + ".jpg"), frame)
+        counter = counter + 1
+        cv2.imshow('frame', frame)
+        cv2.waitKey(100)
+    print("Number of frames", str(counter))
+    
+video.release()
+cv2.destroyAllWindows()
+
+"""import os
+import cv2
+
+video_path = os.path.join(".", "wife.mp4")
+video = cv2.VideoCapture(video_path)
+print(type(video))
+
+ret = True
+while ret:
+    ret, frame = video.read()
+
+    if ret:
+        cv2.imshow('frame', frame)
+        cv2.waitKey(40)
+    
+video.release()
+cv2.destroyAllWindows()
+"""
+"""import cv2
 import os
 
 img_path = os.path.join(".", "image.jpg")
@@ -10,3 +49,4 @@ cv2.imshow('image', image)
 cv2.imshow('new_image', new_image)
 
 cv2.waitKey(0)
+"""
